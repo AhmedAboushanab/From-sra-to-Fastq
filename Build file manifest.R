8afe6bc25b131656b93bf6c3d1e2058ee6b38cb1
Ahmed Adel Aboushanab <35786125+AhmedAboushanab@users.noreply.github.com>
2021-01-23 22:55:46 +0200

    Add files via upload

---
+++ Build file manifest.R
@@ -0,0 +1,16 @@
+#Set up directory 
+Direction <- read.delim('Direction', header = F)
+File_names <-read.delim('sing10st.txt', header = F)
+paths <- read.delim('listOfFiles.list', header = F)
+#Combine all the files into one file
+total <- data.frame(File_names, paths, Direction)
+#Transform the dataframe into an array to remove the row numbers from the file
+total2 <- array(total)
+#build a headless csv file
+write.csv(total2, file = 'file_manifest.csv', row.names = F)
+#read the headless csv file 
+File <- read.csv("file_manifest.csv", header = F)
+#set column names to the file
+colnames(File) <- c("sample-id", 'Absolute-filepath', 'direction')
+#build a csv file with column names
+write.csv(File, file = 'file_manifest2.csv', row.names = F)
