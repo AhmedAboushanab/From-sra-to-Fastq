b81f38816eccfbce84507ca1fef3af5b34a8b376
Ahmed Adel Aboushanab <35786125+AhmedAboushanab@users.noreply.github.com>
2021-01-23 17:31:33 +0200

    Update Converting list of files from sra to fastq.R

--- Converting list of files from sra to fastq.R
+++ Converting list of files from sra to fastq.R
@@ -1,6 +1,6 @@
 File_names<-data.frame(read.delim("file_name.txt"))
 for(f in 1:nrow(File_names)) {
-  g = sri[f, 1]
+  g = File_names[f, 1]
   cmd = paste("fastq-dump --split-3", g)
   cat(cmd,"\n")#print the current command
   system(cmd) # invoke command
