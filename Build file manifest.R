388256f8916c2f5b2c861b1a20716e3de1955118
Ahmed Adel Aboushanab <35786125+AhmedAboushanab@users.noreply.github.com>
2021-02-03 20:14:32 +0200

    Update Build file manifest.R

--- Build file manifest.R
+++ Build file manifest.R
@@ -1,19 +1,38 @@
-#Set up directory 
-Direction <- read.delim('Direction', header = F)
-File_names <-read.delim('sample-ids.txt', header = F)
-paths <- read.delim('listOfFiles.list', header = F)
-#Combine all the files into one file
-total <- data.frame(File_names, paths, Direction)
-#Transform the dataframe into an array to remove the row numbers from the file
-total2 <- array(total)
-#build a headless csv file
-write.csv(total2, file = 'file_manifest.csv', row.names = F)
-#read the headless csv file 
-File <- read.csv("file_manifest.csv", header = F)
-#set column names to the file
-colnames(File) <- c("sample-id", 'Absolute-filepath', 'direction')
-#build a csv file with column names
-write.csv(File, file = 'file_manifest2.csv', row.names = F)
-#Remove the headless csv file
-cmd <-paste('rm file_manifest.csv')
+#production of list of first list of files
+system(paste('cd path to the folder with the fastq data'))
+system(paste('ls *.fastq > file-id.tsv'))
+system(paste('ls -d "$PWD"/*_1.fastq > listF.tsv'))
+system(paste('ls -d "$PWD"/*_2.fastq > listR.tsv'))
+
+#production of sample-id file
+library(stringr)
+File_names <-data.frame(read.delim('file-id.tsv', header = F))
+names_list = list()
+for(f in 1:nrow(File_names)){
+  g = File_names[f, 1]
+  if(str_detect(g, "_1.fastq") == TRUE) {
+    new = gsub("_1.fastq", "", g) #removes a pattern from file name 
+    names_list = append(names_list, new)
+  }else if(str_detect(g, "_2.fastq") == TRUE) {
+    new = gsub("_2.fastq", "", g)
+  } else {
+    break
+  }
+}
+print(names_list)
+#write(unlist(names_list), file = 'sample-id', sep = '\n')
+write('sample-id', file = 'sample-id.tsv', sep = '\n')
+write(unlist(names_list), file = 'sample-id.tsv', append = T)
+
+#Prpduction of Forward_final_paths.tsv and Reverse_final_paths.tsv
+#Forward
+write('forward-absolute-filepath', file = 'Forward_paths_list.tsv', sep = '\n')
+cmd <- paste('cat Forward_paths_list.tsv listF.tsv > Forward_final_paths.tsv')
 system(cmd)
+#Reverse
+write('reverse-absolute-filepath', file = 'Reverse_paths_list.tsv', sep = '\n')
+cmd <- paste('cat Reverse_paths_list.tsv listR.tsv > Reverse_final_paths.tsv')
+system(cmd)
+
+#Filemanifest 
+system(paste('paste sample-id.tsv Forward_final_paths.tsv Reverse_final_paths.tsv > manifest.tsv'))
